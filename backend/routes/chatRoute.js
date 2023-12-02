0798b094db36e23d40f87bef0eabe65ec61aa8ca
Abe-Tiz <106752541+Abe-Tiz@users.noreply.github.com>
2023-12-02 11:19:21 +0300

    add chat message

--- backend/routes/chatRoute.js
+++ backend/routes/chatRoute.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const { protect } = require('../middleware/authMiddleware');
-const { accessChat } = require('../controllers/ChatController');
+const { accessChat, fetchChats } = require("../controllers/ChatController");
 
 const router = express.Router();
 
 router.route("/").post(protect, accessChat);
-// router.route("/").get(protect, fetchChat);
+router.route("/").get(protect, fetchChats);
 // router.route("/group").post(protect, createGroup);
 // router.route("/rename").put(protect, renameGroup);
 // router.route("/groupremove").put(protect, removeFromGroup);
