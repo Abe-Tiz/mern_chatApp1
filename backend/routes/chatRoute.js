22460a9ee9c10b0c6b7472a8fc85cb65c8be2b83
Abe-Tiz <106752541+Abe-Tiz@users.noreply.github.com>
2023-12-02 11:37:10 +0300

    create group chat

--- backend/routes/chatRoute.js
+++ backend/routes/chatRoute.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const { protect } = require('../middleware/authMiddleware');
-const { accessChat, fetchChats } = require("../controllers/ChatController");
+const { accessChat, fetchChats, createGroupChat } = require("../controllers/ChatController");
 
 const router = express.Router();
 
 router.route("/").post(protect, accessChat);
 router.route("/").get(protect, fetchChats);
-// router.route("/group").post(protect, createGroup);
+router.route("/group").post(protect, createGroupChat);
 // router.route("/rename").put(protect, renameGroup);
 // router.route("/groupremove").put(protect, removeFromGroup);
 // router.route("/groupadd").put(protect, addToGroup);
